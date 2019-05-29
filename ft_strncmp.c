3b76232043ddc4e8a6f7a71d9532a9cf7e65e4a2
jadonhansen <jadongavhansen@gmail.com>
2019-05-29 18:35:56 +0200

    Corrected most functions using libfttest

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,12 +15,20 @@
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int i;
+	unsigned char *str1;
+	unsigned char *str2;
 
 	i = 0;
-	while ((s1[i] == s2[i] && s1[i] != '\0'
-			&& s2[i] != '\0' && ((size_t)i < n - 1)))
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (n > 0)
 	{
-		i++;
+		while ((str1[i] == str2[i] && str1[i] != '\0'
+				&& str2[i] != '\0' && ((size_t)i < n - 1)))
+		{
+			i++;
+		}
+		return (str1[i] - str2[i]);
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
