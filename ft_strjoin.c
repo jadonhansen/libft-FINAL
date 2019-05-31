2907a0bd44929f96f8f76b9a75736d27d4672763
jadonhansen <jadongavhansen@gmail.com>
2019-05-31 14:50:12 +0200

    Done strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 14:05:55 by jhansen           #+#    #+#             */
+/*   Updated: 2019/05/31 14:44:57 by jhansen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *string;
+	size_t i;
+
+	i = 0;
+	string = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	while (i < ft_strlen(s1))
+	{
+		string[i] = s1[i];
+		i++;
+	}
+	string = ft_strcat(string, s2);
+	if (string == NULL)
+		return (NULL);
+	return (string);
+}
